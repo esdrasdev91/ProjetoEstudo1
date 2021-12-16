92132c6922a0ee3884801974638246ee3e338260
PROGRAMMER <PROGRAMMER@Esdras-Desktop>
2021-12-16 18:48:49 -0300

    Criação do projeto

---
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executavél em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve Texto no console*/
+		System.out.println("Meu primeiro programa em Java - do curso formação Java web");
+		System.out.println("Java é orientado a Objetos");
+	}
+
+}
